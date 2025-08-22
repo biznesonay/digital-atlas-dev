50bb729f0883f425b2d5b2eb27eae2846c9ccbd5
biznesonay <biznesonay@gmail.com>
2025-08-23 01:20:19 +0500

    fix: improve object tracking and import reliability

---
+++ lib/logger.ts
@@ -0,0 +1,7 @@
+const logger = {
+  info: (message: string, ...args: unknown[]) => console.info(message, ...args),
+  warn: (message: string, ...args: unknown[]) => console.warn(message, ...args),
+  error: (message: string, ...args: unknown[]) => console.error(message, ...args)
+}
+
+export default logger
